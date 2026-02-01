7e70987d5ec711467c2f1de2659dde8f6eba6658
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-02-01 17:17:55 +0600

    applied function theory

---
+++ parameter.js
@@ -0,0 +1,11 @@
+function sum() {
+  console.log("sum function is callded");
+}
+sum();
+
+function summation(num1, num2) {
+  const addition = num1 + num2;
+  console.log(addition);
+}
+summation(11, 13);
+summation(10, 20);
