7dc42df2534b00f8355a6751f755cce5ff92c96e
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-02-05 14:05:53 +0600

    applied function return  theory

---
+++ conditional.js
@@ -0,0 +1,20 @@
+function isEven(num) {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+}
+// console.log(isEven(45));
+
+/* Do sum of an array */
+
+let numbers = [2, 2, 6, 7];
+function doSum(number) {
+  let sum = 0;
+  for (number of numbers) {
+    sum += number;
+  }
+  return sum;
+}
+let total = doSum(numbers);
+console.log(total);
